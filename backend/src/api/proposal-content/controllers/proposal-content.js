35cc66b33904e5e7ae77ca8aa574e2e48a0697e1
Vukasin Paunovic <vukasinpaunovich@gmail.com>
2024-04-30 16:52:51 +0200

    Add govtool action type when getting the proposal content based on gov action type id

--- backend/src/api/proposal-content/controllers/proposal-content.js
+++ backend/src/api/proposal-content/controllers/proposal-content.js
@@ -1,9 +1,44 @@
-'use strict';
+// @ts-nocheck
+"use strict";
 
 /**
  * proposal-content controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::proposal-content.proposal-content');
+module.exports = createCoreController(
+	"api::proposal-content.proposal-content",
+	({ strapi }) => ({
+		async find(ctx) {
+			const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+
+			const { results, pagination } = await strapi
+				.service("api::proposal-content.proposal-content")
+				.find(sanitizedQueryParams);
+
+			// Get the gov_action_type for each proposal
+			const govActionTypes = await strapi.entityService.findMany(
+				"api::governance-action-type.governance-action-type",
+				{
+					filters: {
+						id: {
+							$in: results.map(
+								(proposal) => proposal.gov_action_type_id
+							),
+						},
+					},
+				}
+			);
+
+			results.forEach((proposal) => {
+				proposal.gov_action_type = govActionTypes.find(
+					(govActionType) =>
+						+govActionType.id === +proposal.gov_action_type_id
+				);
+			});
+
+			return this.transformResponse(results, { pagination });
+		},
+	})
+);
