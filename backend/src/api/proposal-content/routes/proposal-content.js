9acc6f04901f6d05f619e88f781f3d7d9505fb16
Nemanja Radulovic <nemanja.radulovic@chainapp.tech>
2024-05-09 17:17:28 +0200

    auth for proposal content

--- backend/src/api/proposal-content/routes/proposal-content.js
+++ backend/src/api/proposal-content/routes/proposal-content.js
@@ -1,15 +1,28 @@
-'use strict';
+//@ts-nocheck
+"use strict";
 
 /**
- * proposal-content router
+ * proposal router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::proposal-content.proposal-content', {
-    config: {
-        create: {
-            roles: ['authenticated', 'public'],
-        }
-    }
+module.exports = createCoreRouter("api::proposal-content.proposal-content", {
+	config: {
+		find: {
+			roles: ["authenticated", "public"],
+		},
+		create: {
+			roles: [],
+		},
+		findOne: {
+			roles: ["authenticated", "public"],
+		},
+		update: {
+			roles: [],
+		},
+		delete: {
+			roles: [],
+		},
+	},
 });
