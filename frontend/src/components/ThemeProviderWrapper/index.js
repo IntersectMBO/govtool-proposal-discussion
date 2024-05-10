e853f03a04018664828dfd66b12f0d0824aa1842
Nemanja Radulovic <nemanja.radulovic@chainapp.tech>
2024-05-10 10:03:31 +0200

    fix for themeProvider

--- frontend/src/components/ThemeProviderWrapper/index.js
+++ frontend/src/components/ThemeProviderWrapper/index.js
@@ -6,9 +6,6 @@ import {
 	createTheme,
 	responsiveFontSizes,
 } from "@mui/material/styles";
-import {Box} from "@mui/material";
-import { Footer } from "@/components";
-import { useEffect, useRef, useState } from "react";
 
 let theme = createTheme({
 	palette: {
@@ -54,57 +51,7 @@ theme = responsiveFontSizes(theme);
 
 
 function ThemeProviderWrapper({ children }) {
-	const mobileNavRef = useRef();
-
-	const [mobileNavHeight, setMobileNavHeight] = useState(0);
-	const [windowWidth, setWindowWidth] = useState(0);
-
-	useEffect(() => {
-		if (window) {
-			setWindowWidth(window?.innerWidth);
-		}
-		const handleResize = () => {
-			setWindowWidth(window?.innerWidth);
-		};
-
-		window?.addEventListener("resize", handleResize);
-
-		return () => window?.removeEventListener("resize", handleResize);
-	}, []);
-
-	useEffect(() => {
-		if (mobileNavRef?.current) {
-			// To set content height the right amount because of mobile navbar
-			setMobileNavHeight(mobileNavRef?.current?.offsetHeight);
-		}
-	}, [mobileNavRef, windowWidth]);
-
-	return (
-		<ThemeProvider theme={theme}>
-			<Box
-				component="main"
-				display={"flex"}
-				flexDirection={"column"}
-				flexGrow={1}
-				sx={{
-					p: 0,
-					backgroundColor: (theme) =>
-						theme.palette.background.default,
-					height: {
-						xs: `calc(100dvh - ${mobileNavHeight}px)`,
-						md: "100dvh",
-					},
-					padding: {
-						xs: `20px`,
-						md: `20px 0 20px 0`,
-					},
-					overflow: "auto",
-				}}
-			>
-				{children}
-				<Footer sx={{ mt: "auto"}} />
-			</Box>
-		</ThemeProvider>);
+	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
 
 export default ThemeProviderWrapper;
