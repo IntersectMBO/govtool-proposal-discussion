a442c907d9f62a7d3fc2ab499ad62ddfefc275eb
teske00 <ntesovc27@gmail.com>
2024-05-21 10:56:25 +0200

    Added is-owner middleware

--- backend/src/api/poll-vote/routes/poll-vote.js
+++ backend/src/api/poll-vote/routes/poll-vote.js
@@ -1,3 +1,4 @@
+//@ts-nocheck
 "use strict";
 
 /**
@@ -6,4 +7,24 @@
 
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter("api::poll-vote.poll-vote");
+module.exports = createCoreRouter("api::poll-vote.poll-vote", {
+  config: {
+    find: {
+      roles: ["authenticated"],
+    },
+    create: {
+      roles: ["authenticated"],
+    },
+    findOne: {
+      roles: [],
+    },
+    update: {
+      roles: [],
+      middlewares: ["global::is-owner"],
+    },
+    delete: {
+      roles: ["authenticated"],
+      middlewares: ["global::is-owner"],
+    },
+  },
+});
