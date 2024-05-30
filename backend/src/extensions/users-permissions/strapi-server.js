95c4cb0dc16043713e95f23014b2dca6e8fc1121
Nemanja Radulovic <nemanja.radulovic@chainapp.tech>
2024-05-30 15:45:27 +0200

    Added govtool_username to user, added update user to update govtool_username

--- backend/src/extensions/users-permissions/strapi-server.js
+++ backend/src/extensions/users-permissions/strapi-server.js
@@ -1,41 +1,41 @@
 // @ts-nocheck
-"use strict";
+'use strict';
 
-const utils = require("@strapi/utils");
+const utils = require('@strapi/utils');
 const { ApplicationError } = utils.errors;
 const { sanitize } = utils;
-const _ = require("lodash");
+const _ = require('lodash');
 
 const sanitizeUser = (user, ctx) => {
   const { auth } = ctx.state;
-  const userSchema = strapi.getModel("plugin::users-permissions.user");
+  const userSchema = strapi.getModel('plugin::users-permissions.user');
 
   return sanitize.contentAPI.output(user, userSchema, { auth });
 };
 
 const getService = (name) => {
-  return strapi.plugin("users-permissions").service(name);
+  return strapi.plugin('users-permissions').service(name);
 };
 
 module.exports = (plugin) => {
   plugin.controllers.auth.callback = async (ctx) => {
-    const provider = ctx.params.provider || "local";
+    const provider = ctx.params.provider || 'local';
     const params = ctx.request.body;
 
-    const store = strapi.store({ type: "plugin", name: "users-permissions" });
-    const grantSettings = await store.get({ key: "grant" });
+    const store = strapi.store({ type: 'plugin', name: 'users-permissions' });
+    const grantSettings = await store.get({ key: 'grant' });
 
-    const grantProvider = provider === "local" ? "email" : provider;
+    const grantProvider = provider === 'local' ? 'email' : provider;
 
-    if (!_.get(grantSettings, [grantProvider, "enabled"])) {
-      throw new ApplicationError("This provider is disabled");
+    if (!_.get(grantSettings, [grantProvider, 'enabled'])) {
+      throw new ApplicationError('This provider is disabled');
     }
 
-    if (provider === "local") {
+    if (provider === 'local') {
       const { identifier } = params;
 
       const user = await strapi
-        .query("plugin::users-permissions.user")
+        .query('plugin::users-permissions.user')
         .findOne({
           where: {
             provider,
@@ -48,18 +48,18 @@ module.exports = (plugin) => {
 
       if (!user) {
         const pluginStore = await strapi.store({
-          type: "plugin",
-          name: "users-permissions",
+          type: 'plugin',
+          name: 'users-permissions',
         });
 
-        const settings = await pluginStore.get({ key: "advanced" });
+        const settings = await pluginStore.get({ key: 'advanced' });
 
         if (!settings.allow_register) {
-          throw new ApplicationError("Register action is currently disabled");
+          throw new ApplicationError('Register action is currently disabled');
         }
 
         const role = await strapi
-          .query("plugin::users-permissions.role")
+          .query('plugin::users-permissions.role')
           .findOne({ where: { type: settings.default_role } });
 
         const identifierFilter = {
@@ -67,33 +67,33 @@ module.exports = (plugin) => {
         };
 
         const conflictingUserCount = await strapi
-          .query("plugin::users-permissions.user")
+          .query('plugin::users-permissions.user')
           .count({
             where: { ...identifierFilter, provider },
           });
 
         if (conflictingUserCount > 0) {
           throw new ApplicationError(
-            "There is already a user with this wallet address."
+            'There is already a user with this wallet address.'
           );
         }
 
         const newUser = {
           username: identifier,
           email: `${identifier}@example.com`,
-          provider: "local",
+          provider: 'local',
           password: identifier,
           role: role.id,
           confirmed: true,
         };
 
-        const user = await getService("user").add(newUser);
+        const user = await getService('user').add(newUser);
 
         const sanitizedUser = await sanitizeUser(user, ctx);
 
         if (settings.email_confirmation) {
           try {
-            await getService("user").sendConfirmationEmail(sanitizedUser);
+            await getService('user').sendConfirmationEmail(sanitizedUser);
           } catch (err) {
             throw new ApplicationError(err.message);
           }
@@ -101,34 +101,64 @@ module.exports = (plugin) => {
           return ctx.send({ user: sanitizedUser });
         }
 
-        const jwt = getService("jwt").issue(_.pick(user, ["id"]));
+        const jwt = getService('jwt').issue(_.pick(user, ['id']));
 
         return ctx.send({
           jwt,
           user: sanitizedUser,
         });
       } else {
-        const advancedSettings = await store.get({ key: "advanced" });
+        const advancedSettings = await store.get({ key: 'advanced' });
         const requiresConfirmation = _.get(
           advancedSettings,
-          "email_confirmation"
+          'email_confirmation'
         );
 
         if (requiresConfirmation && user.confirmed !== true) {
-          throw new ApplicationError("Your account email is not confirmed");
+          throw new ApplicationError('Your account email is not confirmed');
         }
 
         if (user.blocked === true) {
           throw new ApplicationError(
-            "Your account has been blocked by an administrator"
+            'Your account has been blocked by an administrator'
           );
         }
 
         return ctx.send({
-          jwt: getService("jwt").issue({ id: user.id }),
+          jwt: getService('jwt').issue({ id: user.id }),
           user: await sanitizeUser(user, ctx),
         });
       }
+    }
+  };
+  plugin.controllers.user.update = async (ctx) => {
+    const params = ctx?.request?.body;
+    const userId = ctx?.state?.user?.id;
+
+    if (!params) {
+      ctx.throw(400, 'Missing parameters for user update.');
+    }
+
+    const { govtoolUsername } = params;
+
+    if (!userId) {
+      ctx.throw(400, 'User not authenticated or missing user ID.');
+    }
+
+    try {
+      const updatedUser = await strapi.entityService.update(
+        'plugin::users-permissions.user',
+        userId,
+        {
+          data: {
+            govtool_username: govtoolUsername,
+          },
+        }
+      );
+
+      return await sanitizeUser(updatedUser, ctx);
+    } catch (error) {
+      ctx.throw(400, 'Failed to update user: ' + error.message);
     }
   };
 
